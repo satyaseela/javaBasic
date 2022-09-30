375cfdef3978c57257fa69c4fdd132e33cc638e3
satya <sherlinsatya@gmail.com>
2022-09-30 07:00:16 +0200

    firstcommit

---
+++ src/javaBasic1/helloworld.java
@@ -0,0 +1,12 @@
+package javaBasic1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("helloworld");
+
+	}
+
+}
